5d221f16e3501b5967337c79c51f238f69d24bb6
Twinkling-G <92655873+Twinkling-G@users.noreply.github.com>
2023-02-20 21:17:23 +0800

    Add files via upload

--- ringTemp.js
+++ ringTemp.js
@@ -1,9 +1,8 @@
 /*
 author:Twinkling
-version:0.1.0
+version:0.2.0
 */
 importPackage(Packages.java.io);
-
 
 importPackage(Packages.com.sk89q.worldedit);
 importPackage(Packages.com.sk89q.worldedit.regions);
@@ -15,13 +14,14 @@ importPackage(Packages.com.sk89q.worldedit.function.operation);
 
 importPackage(Packages.com.sk89q.jnbt);
 
-var usage = "ringTemp <outer_x_diameter> <outer_z_diameter> [--flags] [schematic_path]\n";
+var usage = "ringTemp <outer_x_radius> <outer_z_radius> [<pos_x_offset> <pos_z_offset>] [--flags] [schematic_path]\n";
 usage += "flags :\n";
 usage += "    o : mark the center.\n";
 usage += "    b : use bigEndian order.\n";
 usage += "    s : save as schematic.\n";
 usage += "    g : do not generate in the world.\n";
-context.checkArgs(2, 4, usage);
+usage += "    a : don't align template center with the x-z grid.\n";
+context.checkArgs(2, 6, usage);
 
 var localSession = context.getSession();
 var world = player.getWorld();
@@ -89,20 +89,22 @@ var sliceHelper = {
 var saveSchematic = false;
 var generateTemplate = true;
 var bigEndian = false;
-var markTheCenter = false;
+var markCenter = false;
+var alignGrid = true;
+var specifiedPosOffset = false;
 
 var argvOffset = 1;
 var otherArgvsLen = argv.length - 1;
 if(otherArgvsLen > 0){
-	if(argv[argvOffset].equals("?")){
+	if(String(argv[argvOffset]).equals("?")){
 		player.print("usage : " + usage);
 		throw "This is a help.\n";
 	}
 }
 
 if(otherArgvsLen > 0){
-	var outerXDiameter = parseInt(argv[argvOffset]);
-	if(!isNaN(outerXDiameter) && outerXDiameter >= 0){
+	var outerXRadius = parseFloat(String(argv[argvOffset]));
+	if(!isNaN(outerXRadius) && outerXRadius >= 0){
 		argvOffset ++;
 		otherArgvsLen --;
 	}else{
@@ -113,8 +115,8 @@ if(otherArgvsLen > 0){
 }
 
 if(otherArgvsLen > 0){
-	var outerZDiameter = parseInt(argv[argvOffset]);
-	if(!isNaN(outerZDiameter) && outerZDiameter >= 0){
+	var outerZRadius = parseFloat(String(argv[argvOffset]));
+	if(!isNaN(outerZRadius) && outerZRadius >= 0){
 		argvOffset ++;
 		otherArgvsLen --;
 	}else{
@@ -124,8 +126,18 @@ if(otherArgvsLen > 0){
 	throw "outer_z_radius must be specified.\n";
 }
 
+if(otherArgvsLen >= 2){
+	var posXOffset = parseFloat(String(argv[argvOffset + 0]));
+	var posZOffset = parseFloat(String(argv[argvOffset + 1]));
+	if(!isNaN(posXOffset) && !isNaN(posZOffset)){
+		specifiedPosOffset = true;
+		argvOffset +=2;
+		otherArgvsLen -=2;
+	}	
+}
+
 if(otherArgvsLen > 0){
-	var flags = String(argv[argvOffset]);
+	var flags = String(String(argv[argvOffset]));
 	if(flags.substring(0,2).equals("--")){
 		if(flags.indexOf("s") != -1){
 			saveSchematic = true;
@@ -137,7 +149,10 @@ if(otherArgvsLen > 0){
 			bigEndian = true;
 		}
 		if(flags.indexOf("o") != -1){
-			markTheCenter = true;
+			markCenter = true;
+		}
+		if(flags.indexOf("a") != -1){
+			alignGrid = false;
 		}
 		argvOffset ++;
 		otherArgvsLen --;
@@ -147,42 +162,55 @@ if(otherArgvsLen > 0){
 
 sliceHelper.init(region, bigEndian);
 
-var air = new BaseBlock(BlockID.AIR)
 var centerBlock = new BaseBlock(BlockID.GOLD_BLOCK);
 var ySize = sliceHelper.getYSize();
 var xSize = sliceHelper.getXSize();
-var pos = localSession.getPlacementPosition(player).floor();
 
-var outerXRadius = outerXDiameter / 2;
-var outerZRadius = outerZDiameter / 2;
-var pos1 = pos.add(
-	outerXDiameter & 0x1 ? -Math.floor(outerXRadius) : -outerXRadius,
-	0, 
-	outerZDiameter & 0x1 ? -Math.floor(outerZRadius) : -outerZRadius
-);
-var pos2 = pos.add(
-	outerXDiameter & 0x1 ? Math.floor(outerXRadius) : outerXRadius - 1,
-	ySize - 1,
-	outerZDiameter & 0x1 ? Math.floor(outerZRadius) : outerZRadius - 1
-); 
-var tempRegion = new CuboidRegion(pos1,pos2);
-var clipboard = new BlockArrayClipboard(tempRegion);
+if(alignGrid){
+	var pos = localSession.getPlacementPosition(player);
+}else{
+	var location = player.getLocation();
+	var pos = new Vector(location.getX(),location.getY(),location.getZ());
+}
+if(specifiedPosOffset){
+	pos = pos.add(posXOffset, 0, posZOffset);
+}
+
+if(saveSchematic){
+	var tolerance = 0.1;
+	var pos1 = pos.add(-outerXRadius, 0, -outerZRadius);
+	pos1 = pos1.floor().add(
+		pos1.getX() - Math.floor(pos1.getX()) > 0.5 + tolerance ? 1 : 0,
+		0,
+		pos1.getZ() - Math.floor(pos1.getZ()) > 0.5 + tolerance ? 1 : 0
+	);
+	var pos2 = pos.add(outerXRadius, ySize - 1, outerZRadius);
+	pos2 = pos2.floor().add(
+		pos2.getX() - Math.floor(pos2.getX()) <= 0.5 - tolerance ? -1 : 0,
+		0,
+		pos2.getZ() - Math.floor(pos2.getZ()) <= 0.5 - tolerance ? -1 : 0
+	);
+	var tempRegion = new CuboidRegion(pos1,pos2);
+	var clipboard = new BlockArrayClipboard(tempRegion);
+}
 
 var beginTime;
 var endTime;
 beginTime = new Date().getTime(); 
 for(var y = 0 ; y < ySize ; y++){
-	var xDiameter = outerXDiameter
-	var zDiameter = outerZDiameter;
+	var xRadius = outerXRadius
+	var zRadius = outerZRadius;
 	for(var x = 0 ; x < xSize ; x++){
 		var block = sliceHelper.getBlock(x, y);
-		makeRing(clipboard, pos, xDiameter, zDiameter, block, false, markTheCenter, centerBlock);
-		if(generateTemplate){
-			makeRing(editSession, pos, xDiameter, zDiameter, block, false, markTheCenter, centerBlock);
+		if(saveSchematic){
+			makeRing(clipboard, pos, xRadius, zRadius, block, false, markCenter, centerBlock);
 		}
-		xDiameter -= 2;
-		zDiameter -= 2;
-		if(xDiameter <= 0 || zDiameter <= 0) break;
+		if(generateTemplate){
+			makeRing(editSession, pos, xRadius, zRadius, block, false, markCenter, centerBlock);
+		}
+		xRadius --;
+		zRadius --;
+		if(xRadius <= 0 || zRadius <= 0) break;
 	}
 	pos = pos.add(0, 1, 0);
 }
@@ -198,6 +226,12 @@ if(saveSchematic){
 			)
 		);
 
+		var format = ClipboardFormat.findByAlias("schematic");
+		var writer = format.getWriter(bos);
+		writer.write(clipboard, world.getWorldData());
+		writer.close();
+		player.print("schematic has stored in \"" + path + "\".\n");
+
 		argvOffset ++;
 		otherArgvsLen --;
 	}else{
@@ -206,118 +240,102 @@ if(saveSchematic){
 	}
 }
 
+function makeRing(extent, pos, xRadius, zRadius, block, lineMode, markCenter, centerBlock){
+	var point = pos.add(xRadius, 0 , 0);
+	point = point.getX() - Math.floor(point.getX()) >= 0.5 ? point.floor() : point.floor().add(-1, 0, 0);
+	point = point.subtract(pos);
+	point = point.add(0.5, 0, 0.5);
 
-if(saveSchematic){
-	var format = ClipboardFormat.findByAlias("schematic");
-	var writer = format.getWriter(bos);
-	writer.write(clipboard, world.getWorldData());
-	writer.close();
-
-	player.print("schematic has stored in \"" + path + "\".\n");
-}
-
-
-function makeRing(extent, pos, xDiameter, zDiameter, block, lineMode, markTheCenter, centerBlock){
-	pos = pos.floor();
-	var xRadius = xDiameter / 2;
-	var zRadius = zDiameter / 2;
-
-	var center = new Vector(0, 0, 0);
-	if(xDiameter & 0x1){
-		center = center.add(0.5, 0, 0);
+	var pointZ = point.getZ();
+	var pointX = point.getX();
+	var fx = pointX / xRadius;
+	var fz = pointZ / zRadius;
+	while(fx * fx + fz * fz > 1){
+		if(pointX <= 0) return;
+		pointX--;
+		fx = pointX / xRadius;
+		fz = pointZ / zRadius;
 	}
-	if(zDiameter & 0x1){
-		center = center.add(0, 0, 0.5);
-	}
+	point = point.setX(pointX);
 
-	if(xDiameter <= 1){
-		var from = (zDiameter & 0x1) ? -Math.floor(zRadius) : -zRadius;
-		var to = (zDiameter & 0x1) ? Math.floor(zRadius) : zRadius - 1;
-		for(var z = from; z <= to ; z ++){
-			extent.setBlock(pos.add(0, 0, z), block);
+	do{
+		pointZ--;
+		fx = pointX / xRadius;
+		fz = pointZ / zRadius;
+	}while(fx * fx + fz * fz <= 1);
+	pointZ++;
+	point = point.setZ(pointZ);
+
+	var startPoint = point;		
+	var dirs = [];
+	var checkedDirs = [];
+	dirs.push(new Vector(0, 0, -1));
+	dirs.push(new Vector(-1, 0, -1)); 
+	dirs.push(new Vector(-1, 0, 0));
+	dirs.push(new Vector(-1, 0, 1));
+	dirs.push(new Vector(0, 0, 1));
+	dirs.push(new Vector(1, 0, 1));
+	dirs.push(new Vector(1, 0, 0));
+	dirs.push(new Vector(1, 0, -1));
+
+	do{
+		extent.setBlock(pos.add(point), block);
+		while(dirs.length > 0){
+			var nextDir = dirs.shift();
+			var nextPoint = point.add(nextDir);
+
+			fx = nextPoint.getX() / xRadius;
+			fz = nextPoint.getZ() / zRadius;
+			if(fx * fx + fz * fz <= 1){
+				break;
+			}else{
+				checkedDirs.push(nextDir);
+			}
 		}
-	}else if(zDiameter <= 1){
-		var from = (xDiameter & 0x1) ? -Math.floor(xRadius) : -xRadius;
-		var to = (xDiameter & 0x1) ? Math.floor(xRadius) : xRadius - 1;
-		for(var x = from; x <= to ; x ++){
-			extent.setBlock(pos.add(x, 0, 0), block);
-		}
-	}else{
-		var bias = new Vector((xDiameter & 0x1) ? 0 : 0.5, 0, (zDiameter & 0x1) ? 0 : 0.5);
-		var pointPP = center.add(-xRadius, 0, 0      ).floor().add(bias);
-		var pointPN = center.add(-xRadius, 0, -0.5   ).floor().add(bias);
-		var pointNP = center.add(xRadius - 1, 0, 0   ).floor().add(bias);
-		var pointNN = center.add(xRadius - 1, 0, -0.5).floor().add(bias);
+		if(dirs.length == 0) break;
 
-		while(0 >= pointPP.getX()){
-			extent.setBlock(pos.add(pointPP), block);
-			extent.setBlock(pos.add(pointPN), block);
-			extent.setBlock(pos.add(pointNP), block);
-			extent.setBlock(pos.add(pointNN), block);
+		if(!lineMode && checkedDirs.length & 0x1){
+			var patchDir = dirs[0];
+			var patchPoint = point.add(patchDir);
+			var x = patchPoint.getX();
+			var z = patchPoint.getZ();
 
-			var dirs = [];
-			dirs.push(new Vector(0, 0, 1)); 
-			dirs.push(new Vector(1, 0, 1));
-			dirs.push(new Vector(1, 0, 0));
-			dirs.push(new Vector(1, 0, -1));
-			dirs.push(new Vector(0, 0, -1));
-
-			while(dirs.length > 0){
-				var dir = dirs.shift();
-				var point = pointPP.add(dir);
-
-				var fx = point.getX() / xRadius;
-				var fz = point.getZ() / zRadius;
-				if(fx * fx + fz * fz <= 1){
-					var xDir = dir.getX();
-					var zDir = dir.getZ();
-					var nextPointPP = pointPP.add(xDir, 0, zDir);
-					var nextPointPN = pointPN.add(xDir, 0, -zDir);
-					var nextPointNP = pointNP.add(-xDir, 0, zDir);
-					var nextPointNN = pointNN.add(-xDir, 0, -zDir);
-					break;
+			fx = x / xRadius;
+			fz = z / zRadius;
+			if(fx * fx + fz * fz <= 1){
+				fx = x / (xRadius - 1);
+				fz = z / (zRadius - 1);
+				if(fx * fx + fz * fz > 1){
+					extent.setBlock(pos.add(patchPoint), block);
 				}
 			}
-
-			if(!lineMode){
-				while(dirs.length > 0){
-					var dir = dirs.shift();
-					var point = pointPP.add(dir);
-					var x = point.getX();
-					var z = point.getZ();
-
-					var fx = x / xRadius;
-					var fz = z / zRadius;
-					if(fx * fx + fz * fz <= 1){
-						fx = x / (xRadius - 1);
-						fz = z / (zRadius - 1);
-						if(fx * fx + fz * fz > 1){
-							var xDir = dir.getX();
-							var zDir = dir.getZ();
-							extent.setBlock(pos.add(pointPP.add(xDir, 0, zDir)), block);
-							extent.setBlock(pos.add(pointPN.add(xDir, 0, -zDir)), block);
-							extent.setBlock(pos.add(pointNP.add(-xDir, 0, zDir)), block);
-							extent.setBlock(pos.add(pointNN.add(-xDir, 0, -zDir)), block);
-						}
-						break;
-					}
-				}
-			}
-			pointPP = nextPointPP;
-			pointPN = nextPointPN; 
-			pointNP = nextPointNP; 
-			pointNN = nextPointNN; 
 		}
-	}
 
-	if(markTheCenter){
+		dirs.unshift(nextDir);
+		if(checkedDirs.length > 0){
+			dirs.unshift(checkedDirs.pop());
+			if(checkedDirs.length > 0){
+				if(checkedDirs.length & 0x1){
+					dirs.unshift(checkedDirs.pop());
+				}
+				while(checkedDirs.length > 0){
+					dirs.push(checkedDirs.shift());
+				}
+			}	
+		}
+		
+		point = nextPoint;
+	}while(!point.equals(startPoint));
+
+	if(markCenter){
+		var center = pos.subtract(pos.floor()); 
 		var fromX = Math.ceil(center.getX() - 1);
 		var toX = Math.floor(center.getX() + 1);
 		var fromZ = Math.ceil(center.getZ() - 1);
 		var toZ = Math.floor(center.getZ() + 1);
 		for(var x = fromX ; x < toX ; x ++){	
 			for(var z = fromZ ; z < toZ ; z ++){
-				extent.setBlock(pos.add(x, 0, z) ,centerBlock);
+				extent.setBlock(pos.add(x, 0, z), centerBlock);
 			}	
 		}
 	}
