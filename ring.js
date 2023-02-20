ef1069bf15754f4653a3ec952e623b4a8248ab27
Twinkling-G <92655873+Twinkling-G@users.noreply.github.com>
2023-02-20 21:19:03 +0800

    Add files via upload

--- ring.js
+++ ring.js
@@ -1,19 +1,21 @@
+/*
+author:Twinkling
+version:0.1.0
+*/
 importPackage(Packages.java.io);
-
-
 importPackage(Packages.com.sk89q.worldedit);
 importPackage(Packages.com.sk89q.worldedit.regions);
 importPackage(Packages.com.sk89q.worldedit.blocks);
 importPackage(Packages.com.sk89q.worldedit.function.pattern);
 importPackage(Packages.com.sk89q.worldedit.extension.factory);
 importPackage(Packages.com.sk89q.worldedit.extension.input);
-
-var usage = "ring <pattern> <outer_x_diameter> <out_z_diameter> [ring_width] [--flags] \n";
+// var log = new BufferedWriter(new FileWriter("jsDebug.txt"));
+var usage = "ring <pattern> <outer_x_radius> <out_z_radius> [ring_width] [<pos_x_offset> <pos_z_offset>] [--flags] [patch_pattern]\n";
 usage += "flags :\n";
 usage += "    l : use line mode.\n";
 usage += "    o : mark the center.\n";
-context.checkArgs(2, 5, usage);
-
+usage += "    a : don't align template center with the x-z grid.\n";
+context.checkArgs(3, 8, usage);
 
 var localSession = context.getSession();
 var world = player.getWorld();
@@ -37,13 +39,16 @@ parserContext.setSession(localSession);
 parserContext.setWorld(world);
 
 var lineMode = false;
-var markTheCenter = false;
+var markCenter = false;
+var alignGrid = true;
+var specifiedPosOffset = false;
+var markThePatchPoints = false;
 var width = 1;
 
 var argvOffset = 1;
 var otherArgvsLen = argv.length - 1;
 if(otherArgvsLen > 0){
-	if(argv[argvOffset].equals("?")){
+	if(String(argv[argvOffset]).equals("?")){
 		player.print("usage : " + usage);
 		throw "This is a help.\n";
 	}
@@ -51,7 +56,7 @@ if(otherArgvsLen > 0){
 
 
 if(otherArgvsLen > 0){
-	var pattern = patternFactory.parseFromInput(argv[argvOffset],parserContext);
+	var pattern = patternFactory.parseFromInput(String(argv[argvOffset]),parserContext);
 	argvOffset ++;
 	otherArgvsLen --;
 }else{
@@ -59,32 +64,32 @@ if(otherArgvsLen > 0){
 }
 
 if(otherArgvsLen > 0){
-	var outerXDiameter = parseInt(argv[argvOffset]);
-	if(!isNaN(outerXDiameter)){
+	var outerXRadius = parseFloat(String(argv[argvOffset]));
+	if(!isNaN(outerXRadius)){
 		argvOffset ++;
 		otherArgvsLen --;
 	}else{
-		throw "outer_x_diameter is invaild.\n";
+		throw "outer_x_radius is invaild.\n";
 	}
 }else{
-	throw "outer_x_diameter must be specified.\n";
+	throw "outer_x_radius must be specified.\n";
 }
 
 
 if(otherArgvsLen > 0){
-	var outerZDiameter = parseInt(argv[argvOffset]);
-	if(!isNaN(outerZDiameter)){
+	var outerZRadius = parseFloat(String(argv[argvOffset]));
+	if(!isNaN(outerZRadius)){
 		argvOffset ++;
 		otherArgvsLen --;
 	}else{
-		throw "out_z_diameter is invaild.\n";
+		throw "out_z_radius is invaild.\n";
 	}
 }else{
-	throw "out_z_diameter must be specified.\n";
+	throw "out_z_radius must be specified.\n";
 }
 
 if(otherArgvsLen > 0){
-	var result = parseInt(argv[argvOffset]);
+	var result = parseInt(String(argv[argvOffset]));
 	if(!isNaN(result)){
 		if(result <= 0){
 			throw "ring_width must be positive.\n"
@@ -95,6 +100,17 @@ if(otherArgvsLen > 0){
 	} 
 }
 
+
+if(otherArgvsLen >= 2){
+	var posXOffset = parseFloat(String(argv[argvOffset + 0]));
+	var posZOffset = parseFloat(String(argv[argvOffset + 1]));
+	if(!isNaN(posXOffset) && !isNaN(posZOffset)){
+		specifiedPosOffset = true;
+		argvOffset +=2;
+		otherArgvsLen -=2;
+	}	
+}
+
 if(otherArgvsLen > 0){
 	var flags = String(argv[argvOffset]);
 	if(flags.substring(0,2).equals("--")){
@@ -102,125 +118,148 @@ if(otherArgvsLen > 0){
 			lineMode = true;
 		}
 		if(flags.indexOf("o") != -1){
-			markTheCenter = true;
+			markCenter = true;
+		}
+		if(flags.indexOf("a") != -1){
+			alignGrid = false;
+		}
+		if(flags.indexOf("p") != -1){
+			markThePatchPoints = true;
 		}
 		argvOffset ++;
 		otherArgvsLen --;
 	}	
 }
 
+if(markThePatchPoints){
+	if(otherArgvsLen > 0){
+		var patchPattern = patternFactory.parseFromInput(String(argv[argvOffset]),parserContext);
+		argvOffset ++;
+		otherArgvsLen --;
+	}else{
+		throw "patch pattern is invaild.\n";
+	}
+}
 
-var pos = localSession.getPlacementPosition(player);
+if(alignGrid){
+	var pos = localSession.getPlacementPosition(player);
+}else{
+	var location = player.getLocation();
+	var pos = new Vector(location.getX(),location.getY(),location.getZ());
+}
+if(specifiedPosOffset){
+	pos = pos.add(posXOffset, 0, posZOffset);
+}
 var centerBlock = new BaseBlock(BlockID.GOLD_BLOCK);
-while(width > 0 && outerXDiameter > 0 && outerZDiameter > 0){
-	makeRing(pos, outerXDiameter, outerZDiameter, lineMode, markTheCenter, centerBlock);
+var patchPattern = markThePatchPoints ? patchPattern : pattern;
+while(width > 0 && outerXRadius > 0 && outerZRadius > 0){
+	makeRing(editSession, pos, outerXRadius, outerZRadius, pattern, patchPattern, lineMode, markCenter, centerBlock);
 	width--;
-	outerXDiameter -= 2;
-	outerZDiameter -= 2;
+	outerXRadius --;
+	outerZRadius --;
 }
 player.print("generated a ring\n");
+// log.close();
 
-function makeRing(pos, xDiameter, zDiameter, lineMode, markTheCenter, centerBlock){
-	pos = pos.floor();
-	var xRadius = xDiameter / 2;
-	var zRadius = zDiameter / 2;
 
-	var center = new Vector(0, 0, 0);
-	if(xDiameter & 0x1){
-		center = center.add(0.5, 0, 0);
+function makeRing(extent, pos, xRadius, zRadius, pattern, patchPattern, lineMode, markCenter, centerBlock){
+	var point = pos.add(xRadius, 0 , 0);
+	point = point.getX() - Math.floor(point.getX()) >= 0.5 ? point.floor() : point.floor().add(-1, 0, 0);
+	point = point.subtract(pos);
+	point = point.add(0.5, 0, 0.5);
+
+	var pointZ = point.getZ();
+	var pointX = point.getX();
+	var fx = pointX / xRadius;
+	var fz = pointZ / zRadius;
+	while(fx * fx + fz * fz > 1){
+		if(pointX <= 0) return;
+		pointX--;
+		fx = pointX / xRadius;
+		fz = pointZ / zRadius;
 	}
-	if(zDiameter & 0x1){
-		center = center.add(0, 0, 0.5);
-	}
+	point = point.setX(pointX);
 
-	if(xDiameter <= 1){
-		var from = zDiameter & 0x1 ? -Math.floor(zRadius) : -zRadius;
-		var to = zDiameter & 0x1 ? Math.floor(zRadius) : zRadius - 1;
-		for(var z = from; z <= to ; z ++){
-			editSession.setBlock(pos.add(0, 0, z), pattern);
+	do{
+		pointZ--;
+		fx = pointX / xRadius;
+		fz = pointZ / zRadius;
+	}while(fx * fx + fz * fz <= 1);
+	pointZ++;
+	point = point.setZ(pointZ);
+
+	var startPoint = point;		
+	var dirs = [];
+	var checkedDirs = [];
+	dirs.push(new Vector(0, 0, -1));
+	dirs.push(new Vector(-1, 0, -1)); 
+	dirs.push(new Vector(-1, 0, 0));
+	dirs.push(new Vector(-1, 0, 1));
+	dirs.push(new Vector(0, 0, 1));
+	dirs.push(new Vector(1, 0, 1));
+	dirs.push(new Vector(1, 0, 0));
+	dirs.push(new Vector(1, 0, -1));
+
+	do{
+		var blockPoint = pos.add(point);
+		extent.setBlock(blockPoint, pattern.apply(blockPoint));
+		while(dirs.length > 0){
+			var nextDir = dirs.shift();
+			var nextPoint = point.add(nextDir);
+
+			fx = nextPoint.getX() / xRadius;
+			fz = nextPoint.getZ() / zRadius;
+			if(fx * fx + fz * fz <= 1){
+				break;
+			}else{
+				checkedDirs.push(nextDir);
+			}
 		}
-	}else if(zDiameter <= 1){
-		var from = xDiameter & 0x1 ? -Math.floor(xRadius) : -xRadius;
-		var to = xDiameter & 0x1 ? Math.floor(xRadius) : xRadius - 1;
-		for(var x = from; x <= to ; x ++){
-			editSession.setBlock(pos.add(x, 0, 0), pattern);
-		}
-	}else{
-		var bias = new Vector(xDiameter & 0x1 ? 0 : 0.5, 0, zDiameter & 0x1 ? 0 : 0.5);
-		var pointPP = center.add(-xRadius, 0, 0      ).floor().add(bias);
-		var pointPN = center.add(-xRadius, 0, -0.5   ).floor().add(bias);
-		var pointNP = center.add(xRadius - 1, 0, 0   ).floor().add(bias);
-		var pointNN = center.add(xRadius - 1, 0, -0.5).floor().add(bias);
+		if(dirs.length == 0) break;
 
-		while(0 >= pointPP.getX()){
-			editSession.setBlock(pos.add(pointPP), pattern);
-			editSession.setBlock(pos.add(pointPN), pattern);
-			editSession.setBlock(pos.add(pointNP), pattern);
-			editSession.setBlock(pos.add(pointNN), pattern);
+		if(!lineMode && checkedDirs.length & 0x1){
+			var patchDir = dirs[0];
+			var patchPoint = point.add(patchDir);
+			var x = patchPoint.getX();
+			var z = patchPoint.getZ();
 
-			var dirs = [];
-			dirs.push(new Vector(0, 0, 1)); 
-			dirs.push(new Vector(1, 0, 1));
-			dirs.push(new Vector(1, 0, 0));
-			dirs.push(new Vector(1, 0, -1));
-			dirs.push(new Vector(0, 0, -1));
-
-			while(dirs.length > 0){
-				var dir = dirs.shift();
-				var point = pointPP.add(dir);
-
-				var fx = point.getX() / xRadius;
-				var fz = point.getZ() / zRadius;
-				if(fx * fx + fz * fz <= 1){
-					var xDir = dir.getX();
-					var zDir = dir.getZ();
-					var nextPointPP = pointPP.add(xDir, 0, zDir);
-					var nextPointPN = pointPN.add(xDir, 0, -zDir);
-					var nextPointNP = pointNP.add(-xDir, 0, zDir);
-					var nextPointNN = pointNN.add(-xDir, 0, -zDir);
-					break;
+			fx = x / xRadius;
+			fz = z / zRadius;
+			if(fx * fx + fz * fz <= 1){
+				fx = x / (xRadius - 1);
+				fz = z / (zRadius - 1);
+				if(fx * fx + fz * fz > 1){
+					blockPoint = pos.add(patchPoint);
+					extent.setBlock(blockPoint, patchPattern.apply(blockPoint));
 				}
 			}
-
-			if(!lineMode){
-				while(dirs.length > 0){
-					var dir = dirs.shift();
-					var point = pointPP.add(dir);
-					var x = point.getX();
-					var z = point.getZ();
-
-					var fx = x / xRadius;
-					var fz = z / zRadius;
-					if(fx * fx + fz * fz <= 1){
-						fx = x / (xRadius - 1);
-						fz = z / (zRadius - 1);
-						if(fx * fx + fz * fz > 1){
-							var xDir = dir.getX();
-							var zDir = dir.getZ();
-							editSession.setBlock(pos.add(pointPP.add(xDir, 0, zDir)), pattern);
-							editSession.setBlock(pos.add(pointPN.add(xDir, 0, -zDir)), pattern);
-							editSession.setBlock(pos.add(pointNP.add(-xDir, 0, zDir)), pattern);
-							editSession.setBlock(pos.add(pointNN.add(-xDir, 0, -zDir)), pattern);
-						}
-						break;
-					}
-				}
-			}
-			pointPP = nextPointPP;
-			pointPN = nextPointPN; 
-			pointNP = nextPointNP; 
-			pointNN = nextPointNN; 
 		}
-	}
 
-	if(markTheCenter){
+		dirs.unshift(nextDir);
+		if(checkedDirs.length > 0){
+			dirs.unshift(checkedDirs.pop());
+			if(checkedDirs.length > 0){
+				if(checkedDirs.length & 0x1){
+					dirs.unshift(checkedDirs.pop());
+				}
+				while(checkedDirs.length > 0){
+					dirs.push(checkedDirs.shift());
+				}
+			}	
+		}
+		
+		point = nextPoint;
+	}while(!point.equals(startPoint));
+
+	if(markCenter){
+		var center = pos.subtract(pos.floor()); 
 		var fromX = Math.ceil(center.getX() - 1);
 		var toX = Math.floor(center.getX() + 1);
 		var fromZ = Math.ceil(center.getZ() - 1);
 		var toZ = Math.floor(center.getZ() + 1);
 		for(var x = fromX ; x < toX ; x ++){	
 			for(var z = fromZ ; z < toZ ; z ++){
-				editSession.setBlock(pos.add(x, 0, z) ,centerBlock);
+				extent.setBlock(pos.add(x, 0, z), centerBlock);
 			}	
 		}
 	}
